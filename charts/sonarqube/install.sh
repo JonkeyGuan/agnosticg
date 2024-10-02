801757dc1b76a1bd7937d2b1a869c560e9e0dfe5
Jonkey Guan <jonkey.guan@gmail.com>
2024-10-02 20:59:11 +0800

    updated

--- charts/sonarqube/install.sh
+++ charts/sonarqube/install.sh
@@ -1,1 +1,3 @@
-helm install sonarqube . -f values.yaml -n sonarqube --create-namespace
+oc create ns sonarqube
+oc -n sonarqube create secret generic custom-ca-certs --from-file=cert-1.crt=custom-certs.crt
+helm install sonarqube . --set initContainers=true --set caCerts.secret=custom-ca-certs -f values.yaml -n sonarqube
